9bc0ae1f19973b15f54666ba1d5e8e0f2ee78305
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-10-25 10:36:51 +1100

    Board rendered to screen, game logic commented out for now

--- index.js
+++ index.js
@@ -1,184 +1,205 @@
 //define gameboard as object
 const gameboard={
     game:[
-        ["_","_","_"],
+        ["X","O","_"],
         ["_","_","_"],
         ["_","_","_"]
     ]
+}
+
+//iterate over gameboard, adding a div to the gameboard div
+const gameboardDiv=document.querySelector(".gameboard");
+for(let i=0;i<3;i++){
+    //create a row and append to gameboardDiv
+    const boardRow=document.createElement("div");
+    gameboardDiv.appendChild(boardRow);
+    boardRow.className="boardRow";
+
+    //in each row create 3 squares
+    for(let j=0;j<3;j++){
+        //create square
+        const square=document.createElement("div");
+        square.textContent=gameboard.game[i][j];
+
+        //target gameboard div and append square
+        
+        boardRow.appendChild(square);
+        
+    }
 }
 
 //Setup boolean to determine if game has been won
 let isWinner=false;
 let winner="";
 
-//play game and ask for prompts until isWinner
-while(!isWinner){
-    //get player 1 input, at the moment they are X
-    let p1x=prompt("Player 1 choose a row num");
-    let p1y=prompt("Player 1 choose a column num");
+// //play game and ask for prompts until isWinner
+// while(!isWinner){
+//     //get player 1 input, at the moment they are X
+//     let p1x=prompt("Player 1 choose a row num");
+//     let p1y=prompt("Player 1 choose a column num");
     
-    //check for empty square with while loop, while getting player input
-    while(gameboard.game[p1x][p1y]=="X" || gameboard.game[p1x][p1y]=="O"){
-        //request again for valid input as this square is full
-        p1x=prompt("Sorry that square has been filled, choose a valid row num");
-        p1y=prompt("Sorry that square has been filled, choose a valid coumn num");
-    }
+//     //check for empty square with while loop, while getting player input
+//     while(gameboard.game[p1x][p1y]=="X" || gameboard.game[p1x][p1y]=="O"){
+//         //request again for valid input as this square is full
+//         p1x=prompt("Sorry that square has been filled, choose a valid row num");
+//         p1y=prompt("Sorry that square has been filled, choose a valid coumn num");
+//     }
 
-    //place square in gameboard.game
-    gameboard.game[p1x][p1y]="X";
+//     //place square in gameboard.game
+//     gameboard.game[p1x][p1y]="X";
     
 
-    //check if p1 wins?
-    //check row
-    let countX=0
-    for(let cell of gameboard.game[p1x]){
-        if(cell=="X"){
-            countX+=1;
-        }
-    }
-    if(countX==3){
-        isWinner=true;
-        winner="player1";
-        break;
-    }
+//     //check if p1 wins?
+//     //check row
+//     let countX=0
+//     for(let cell of gameboard.game[p1x]){
+//         if(cell=="X"){
+//             countX+=1;
+//         }
+//     }
+//     if(countX==3){
+//         isWinner=true;
+//         winner="player1";
+//         break;
+//     }
 
-    //check column
-    countX=0
-    for (let i=0;i<3;i++){
-        if(gameboard.game[i][p1y]=="X"){
-            countX+=1;
-        }
-    }
-    if(countX==3){
-        isWinner=true;
-        winner="player1";
-        break;
-    }
+//     //check column
+//     countX=0
+//     for (let i=0;i<3;i++){
+//         if(gameboard.game[i][p1y]=="X"){
+//             countX+=1;
+//         }
+//     }
+//     if(countX==3){
+//         isWinner=true;
+//         winner="player1";
+//         break;
+//     }
 
-    //check diag
-    if(
-        (
-            gameboard.game[0][0]=="X" &&
-            gameboard.game[1][1]=="X" &&
-            gameboard.game[2][2]=="X"
-        ) ||
-        (
-            gameboard.game[0][2]=="X" &&
-            gameboard.game[1][1]=="X" &&
-            gameboard.game[2][2]=="X"
-        )
-    ){
-        isWinner=true;
-        winner="player1";
-        break;
-    }
+//     //check diag
+//     if(
+//         (
+//             gameboard.game[0][0]=="X" &&
+//             gameboard.game[1][1]=="X" &&
+//             gameboard.game[2][2]=="X"
+//         ) ||
+//         (
+//             gameboard.game[0][2]=="X" &&
+//             gameboard.game[1][1]=="X" &&
+//             gameboard.game[2][2]=="X"
+//         )
+//     ){
+//         isWinner=true;
+//         winner="player1";
+//         break;
+//     }
 
-    //Ckeck if board full and declare draw as p1 did not win
-    let countEmpty=0;
-    for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-            if(gameboard.game[i][j]=="_"){
-                countEmpty+=1;
-            }
-        }
-    }
+//     //Ckeck if board full and declare draw as p1 did not win
+//     let countEmpty=0;
+//     for(let i=0;i<3;i++){
+//         for(let j=0;j<3;j++){
+//             if(gameboard.game[i][j]=="_"){
+//                 countEmpty+=1;
+//             }
+//         }
+//     }
 
-    if(countEmpty==0){
-        isWinner=true;
-        winner="Draw";
-        break;
-    }
+//     if(countEmpty==0){
+//         isWinner=true;
+//         winner="Draw";
+//         break;
+//     }
 
-    alert(countX);
-    alert(`${gameboard.game[0][0]}|${gameboard.game[0][1]}|${gameboard.game[0][2]}\n${gameboard.game[1][0]}|${gameboard.game[1][1]}|${gameboard.game[1][2]}\n${gameboard.game[2][0]}|${gameboard.game[2][1]}|${gameboard.game[2][2]}`);
+//     alert(countX);
+//     alert(`${gameboard.game[0][0]}|${gameboard.game[0][1]}|${gameboard.game[0][2]}\n${gameboard.game[1][0]}|${gameboard.game[1][1]}|${gameboard.game[1][2]}\n${gameboard.game[2][0]}|${gameboard.game[2][1]}|${gameboard.game[2][2]}`);
 
-    //Now get player 2 input, at the moment they are naughts
-    let p2x=prompt("Player 2 choose a row num");
-    let p2y=prompt("Player 2 choose a column num");
+//     //Now get player 2 input, at the moment they are naughts
+//     let p2x=prompt("Player 2 choose a row num");
+//     let p2y=prompt("Player 2 choose a column num");
 
-    //check for empty square with while loop, while getting player input
-    while(gameboard.game[p2x][p2y]=="X" || gameboard.game[p2x][p2y]=="O"){
-        //request again for valid input as this square is full
-        p2x=prompt("Sorry that square has been filled, choose a valid row num");
-        p2y=prompt("Sorry that square has been filled, choose a valid column num");
-    }
+//     //check for empty square with while loop, while getting player input
+//     while(gameboard.game[p2x][p2y]=="X" || gameboard.game[p2x][p2y]=="O"){
+//         //request again for valid input as this square is full
+//         p2x=prompt("Sorry that square has been filled, choose a valid row num");
+//         p2y=prompt("Sorry that square has been filled, choose a valid column num");
+//     }
 
-    //place square in gameboard.game
-    gameboard.game[p2x][p2y]="O";
-
-
-    //check if p2 wins?
-    //check row
-    let countY=0
-    for(let cell of gameboard.game[p2x]){
-        if(cell=="O"){
-            countY+=1;
-        }
-    }
-    if(countY==3){
-        isWinner=true;
-        winner="player2";
-        break;
-    }
-
-    //check column
-    countY=0
-    for (let i=0;i<3;i++){
-        if(gameboard.game[i][p2y]=="O"){
-            countY+=1;
-        }
-    }
-    if(countY==3){
-        isWinner=true;
-        winner="player1";
-        break;
-    }
-
-    //check diag
-    if(
-        (
-            gameboard.game[0][0]=="Y" &&
-            gameboard.game[1][1]=="Y" &&
-            gameboard.game[2][2]=="Y"
-        ) ||
-        (
-            gameboard.game[0][2]=="Y" &&
-            gameboard.game[1][1]=="Y" &&
-            gameboard.game[2][2]=="Y"
-        )
-    ){
-        isWinner=true;
-        winner="player2"
-    }
-
-    //Ckeck if board full and declare draw as p2 did not win
-    countEmpty=0;
-    for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-            if(gameboard.game[i][j]=="_"){
-                countEmpty+=1;
-            }
-        }
-    }
-
-    if(countEmpty==0){
-        isWinner=true;
-        winner="Draw";
-        break;
-    }
+//     //place square in gameboard.game
+//     gameboard.game[p2x][p2y]="O";
 
 
-    console.log(gameboard.game);
-    alert(`${gameboard.game[0][0]}|${gameboard.game[0][1]}|${gameboard.game[0][2]}\n${gameboard.game[1][0]}|${gameboard.game[1][1]}|${gameboard.game[1][2]}\n${gameboard.game[2][0]}|${gameboard.game[2][1]}|${gameboard.game[2][2]}`);
-    alert(countY);
+//     //check if p2 wins?
+//     //check row
+//     let countY=0
+//     for(let cell of gameboard.game[p2x]){
+//         if(cell=="O"){
+//             countY+=1;
+//         }
+//     }
+//     if(countY==3){
+//         isWinner=true;
+//         winner="player2";
+//         break;
+//     }
 
-}
+//     //check column
+//     countY=0
+//     for (let i=0;i<3;i++){
+//         if(gameboard.game[i][p2y]=="O"){
+//             countY+=1;
+//         }
+//     }
+//     if(countY==3){
+//         isWinner=true;
+//         winner="player1";
+//         break;
+//     }
 
-if(winner!="Draw"){
-    console.log(`Well done ${winner} you won`);
-}
-else{
-    console.log(`Draw, no one wins!`);
-}
+//     //check diag
+//     if(
+//         (
+//             gameboard.game[0][0]=="Y" &&
+//             gameboard.game[1][1]=="Y" &&
+//             gameboard.game[2][2]=="Y"
+//         ) ||
+//         (
+//             gameboard.game[0][2]=="Y" &&
+//             gameboard.game[1][1]=="Y" &&
+//             gameboard.game[2][2]=="Y"
+//         )
+//     ){
+//         isWinner=true;
+//         winner="player2"
+//     }
+
+//     //Ckeck if board full and declare draw as p2 did not win
+//     countEmpty=0;
+//     for(let i=0;i<3;i++){
+//         for(let j=0;j<3;j++){
+//             if(gameboard.game[i][j]=="_"){
+//                 countEmpty+=1;
+//             }
+//         }
+//     }
+
+//     if(countEmpty==0){
+//         isWinner=true;
+//         winner="Draw";
+//         break;
+//     }
+
+
+//     console.log(gameboard.game);
+//     alert(`${gameboard.game[0][0]}|${gameboard.game[0][1]}|${gameboard.game[0][2]}\n${gameboard.game[1][0]}|${gameboard.game[1][1]}|${gameboard.game[1][2]}\n${gameboard.game[2][0]}|${gameboard.game[2][1]}|${gameboard.game[2][2]}`);
+//     alert(countY);
+
+// }
+
+// if(winner!="Draw"){
+//     console.log(`Well done ${winner} you won`);
+// }
+// else{
+//     console.log(`Draw, no one wins!`);
+// }
 
 
